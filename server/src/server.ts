38bd63f03a7a42a7c0b908dc292f1324e3fbb7b9
princenyamadi <=>
2024-01-06 00:38:03 +0000

    wip server

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,6 +2,7 @@ import * as dotenv from "dotenv";
 import cors from "cors";
 import express from "express";
 import { connectToDatabase } from "./database";
+import { employeeRouter } from "./employee.routes";
 
 
 // load environment variables from .env file, where the ATLAS_URI is configured
@@ -24,9 +25,10 @@ connectToDatabase(ATLAS_URI).then(()=>{
     app.use(cors());
 
     // start the Express server
-    app.listen(5200,()=>{
-        console.log(`Server running at http:localhost:5200...`);
-    });
+    // app.listen(5200,()=>{
+    //     console.log(`Server running at http:localhost:5200...`);
+    // });
+    app.use("/employees", employeeRouter);
 }).catch(error => console.error(error));
 
 // * to run the app use
